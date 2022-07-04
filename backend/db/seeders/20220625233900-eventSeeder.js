92787dff8696d1c2f0f8f19f190070fa50f5b966
Krista Strucke <kstrucke@gmail.com>
2022-07-04 12:51:41 -0700

    fixed seeder

--- backend/db/seeders/20220625233900-eventSeeder.js
+++ backend/db/seeders/20220625233900-eventSeeder.js
@@ -6,13 +6,16 @@ const d2 = "So they're finally here, performing for you, If you know the words, 
 const img2 = "https://i.dailymail.co.uk/i/pix/2010/02/10/article-1249824-083527F7000005DC-550_634x422.jpg";
 const d3 = "Come all ye anime nerds to see anime stuff";
 const img3 = "https://i.pinimg.com/736x/66/9a/9c/669a9cba35f2f183b4f7f50acca73b68.jpg";
+const d4 = "Watch five competitive eaters take on Kirby in a hotdog eating contest. How many hotdogs can they eat in 15 minutes?";
+const img4 = "https://c.tenor.com/uWsVgFo8Z9cAAAAM/kirby-food.gif";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
       return queryInterface.bulkInsert('Events', [
         {hostId: 1, venueId: 1, categoryId: 2, name: "Smash Ultimate Tournament", date: new Date('December 17, 2022 18:30:00'), capacity: 40, image: img1, description: d1 },
-        {hostId: 1, venueId: 3, categoryId: 2, name: "Monkey Dancers", date: new Date('November 17, 2022 19:00:00'), capacity: 200, image: img2, description: d2 },
-        {hostId: 1, venueId: 2, categoryId: 2, name: "AnimeNext Convention", date: new Date('July 17, 2023 08:00:00'), capacity: 2000, image: img3, description: d3 }
+        {hostId: 1, venueId: 3, categoryId: 3, name: "Monkey Dancers", date: new Date('November 17, 2022 19:00:00'), capacity: 200, image: img2, description: d2 },
+        {hostId: 1, venueId: 2, categoryId: 1, name: "AnimeNext Convention", date: new Date('July 17, 2023 08:00:00'), capacity: 2000, image: img3, description: d3 },
+        {hostId: 1, venueId: 1, categoryId: 2, name: "Kirby's Eating Contest", date: new Date('August 11, 2022 08:00:00'), capacity: 250, image: img4, description: d4 }
       ], {});
   },
 
